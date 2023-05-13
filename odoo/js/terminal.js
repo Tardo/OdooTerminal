ae19c77602d58245a5f47906fa7421695f093766
Alexandre D. Díaz <dev@redneboa.es>
2023-05-14 01:39:19 +0200

    [REL] Version 9.3.3

--- odoo/js/terminal.js
+++ odoo/js/terminal.js
@@ -21,7 +21,7 @@ odoo.define("terminal.Terminal", function (require) {
     const _lt = core._lt;
 
     const Terminal = Widget.extend({
-        VERSION: "9.3.2",
+        VERSION: "9.3.3",
 
         MODES: {
             BACKEND_NEW: 1,
