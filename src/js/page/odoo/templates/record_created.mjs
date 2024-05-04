e10980a1115d37fba43264a538fea93d8696631d
Alexandre D. Díaz <dev@redneboa.es>
2024-05-04 05:06:02 +0200

    fix: translations

--- src/js/page/odoo/templates/record_created.mjs
+++ src/js/page/odoo/templates/record_created.mjs
@@ -9,7 +9,7 @@ export default function (model: string, new_ids: $ReadOnlyArray<number>): string
   const links = new_ids?.map(
     nid => `<span class='o_terminal_click o_terminal_cmd' data-cmd='view ${model} ${nid}'>${nid}</span>`,
   );
-  return i18n.t('odoo.templates.recordCreated.sucess', '{{model}} record(s) created successfully: {{links}}', {
+  return i18n.t('odoo.templates.recordCreated.sucess', '{{model}} record(s) created successfully: {{- links}}', {
     model: model,
     links: links.join(', '),
   });
