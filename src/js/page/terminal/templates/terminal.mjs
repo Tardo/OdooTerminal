31d2e9cea6eab79be705ba859d0f781d7ce2d580
Alexandre D. Díaz <dev@redneboa.es>
2024-08-12 03:44:53 +0200

    imp: css

--- src/js/page/terminal/templates/terminal.mjs
+++ src/js/page/terminal/templates/terminal.mjs
@@ -7,7 +7,7 @@ export default function (): string {
     "<div id='terminal' class='o_terminal'>" +
     "<div class='terminal-screen-info-zone'>" +
     "<span class='terminal-screen-running-cmds' id='terminal_running_cmd_count'></span>" +
-    "<div class='btn btn-sm btn-dark border-warning mr-5 terminal-screen-icon-reload-shell p-2' role='button' title='Reload Shell'>" +
+    "<div class='btn btn-sm btn-dark border-warning mr-5 me-5 terminal-screen-icon-reload-shell p-2' role='button' title='Reload Shell'>" +
     "<i class='fa fa-refresh'></i>" +
     '</div>' +
     "<div class='btn btn-sm btn-dark terminal-screen-icon-maximize p-2 rounded-left' role='button' title='Maximize'>" +
