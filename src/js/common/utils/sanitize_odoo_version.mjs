73a21af0c7c433ce4c0ffe1d220a6482ad5524d5
Alexandre D. Díaz <dev@redneboa.es>
2024-10-25 03:19:10 +0200

    fix: common: utils: sanitize_odoo_version

--- src/js/common/utils/sanitize_odoo_version.mjs
+++ src/js/common/utils/sanitize_odoo_version.mjs
@@ -2,7 +2,7 @@
 // Copyright  Alexandre Díaz <dev@redneboa.es>
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-const regexVersion = /[a-z]+(?:\d+)?|[~+]/g;
+const regexVersion = /[a-z]+~|-\d+/g;
 export default function (ver: string): string {
   return ver?.replace(regexVersion, '');
 }
